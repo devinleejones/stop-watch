5a1e225939697703534d7b0ae1957115b29ba9c6
Devin Lee-Jones <devinleejones@gmail.com>
2018-08-30 10:41:10 -0700

    Call update function on a one second interval when the button is clicked#1

--- user-can-start-the-stop-watch/main.js
+++ user-can-start-the-stop-watch/main.js
@@ -2,10 +2,13 @@ var $myButton = document.querySelector('button')
 var $count = document.querySelector('h3')
 
 function changeDigit() {
-  var addInterval = parseInt($count.textContent, 10)
-  addInterval += 1
-  $count.textContent = addInterval
-  console.log(addInterval)
+  var elapsedTime = parseInt($count.textContent, 10)
+  elapsedTime += 1
+  $count.textContent = elapsedTime
 }
 
-$myButton.addEventListener('click', changeDigit)
+function changeInterval() {
+  setInterval(changeDigit, 1000)
+}
+
+$myButton.addEventListener('click', changeInterval)
