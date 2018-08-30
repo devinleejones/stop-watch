21702e25603614db158afa7523dd07e2aa415e29
Devin Lee-Jones <devinleejones@gmail.com>
2018-08-30 10:05:01 -0700

    Define a function that updates the elapsed seconds and writes it to the DOM#1

--- user-can-start-the-stop-watch/main.js
+++ user-can-start-the-stop-watch/main.js
@@ -1,5 +1,11 @@
 var $myButton = document.querySelector('button')
 var $count = document.querySelector('h3')
 
+function changeDigit() {
+  var addInterval = parseInt($count.textContent, 10)
+  addInterval += 1
+  $count.textContent = addInterval
+  console.log(addInterval)
+}
 
-$myButton.addEventListener('click')
+$myButton.addEventListener('click', changeDigit)
