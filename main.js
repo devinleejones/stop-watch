aba2d18aee35b4fef3f84221f7dc4e9e45105a7c
Devin Lee-Jones <devinleejones@gmail.com>
2018-08-30 22:54:40 -0700

    Add pause icon for switches to the state of the watch as well as background color#2

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 var $myButton = document.querySelector('button')
 var $count = document.querySelector('h3')
-var countTime = 0
+var countTime = false
 var intervalID;
 
 function changeDigit() {
   var elapsedTime = parseInt($count.textContent, 10)
   if (countTime === true) {
-  elapsedTime += 1
-  $count.textContent = elapsedTime
+    elapsedTime += 1 //
+    $count.textContent = elapsedTime
   }
 }
 
 function changeInterval() {
-  countTime = !countTime
-  clearInterval(intervalID)
+  countTime = !countTime //toggles between on and off through boolean
+  clearInterval(intervalID) //stops the interval regardless if running or not
   intervalID = setInterval(changeDigit, 1000)
+  //runs changeDigit() every 1 sec and sets result equlal to intervalID
+
+  if (countTime === true) {
+    $myButton.textContent = "Pause"
+    $myButton.classList.toggle("newButtonColor")
+  } else {
+    $myButton.textContent = "Start"
+    $myButton.classList.toggle("newButtonColor")
+  }
 }
 
 $myButton.addEventListener('click', changeInterval)
