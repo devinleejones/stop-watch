4dd2d8129ec59f3c2c5f2ae683544ee1ed308dbb
Devin Lee-Jones <devinleejones@gmail.com>
2018-09-03 14:48:45 -0700

    Add a hidden reset button to the page

---
+++ main.js
@@ -0,0 +1,24 @@
+var $myButton = document.querySelector('button')
+var $watchNumber = document.querySelector('h3')
+var currentTime = false
+var intervalID;
+ function changeDigit() {
+  var elapsedTime = parseInt($watchNumber.textContent, 10)
+  if (currentTime === true) {
+  elapsedTime += 1
+  $watchNumber.textContent = elapsedTime
+  }
+}
+ function changeInterval() {
+  currentTime = !currentTime
+  clearInterval(intervalID)
+  intervalID = setInterval(changeDigit, 1000)
+  if (currentTime === true) {
+    $myButton.textContent = "Pause"
+    $myButton.classList.toggle("newButtonColor")
+  } else {
+    $myButton.textContent = "Start"
+    $myButton.classList.toggle("newButtonColor")
+  }
+}
+ $myButton.addEventListener('click', changeInterval)
